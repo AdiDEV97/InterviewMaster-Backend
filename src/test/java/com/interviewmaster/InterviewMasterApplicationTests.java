0aa529c59036faa2d8fae2a4c4770753fb55b617
Adesh <adeshvaidya97@gmail.com>
2023-08-07 14:46:19 +0530

    Start new Project - Interview-Master

---
+++ src/test/java/com/interviewmaster/InterviewMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
