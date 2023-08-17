76f63642ad11166914310c843cf3d69865a0b882
Adesh <adeshvaidya97@gmail.com>
2023-08-17 15:58:23 +0530

    CorsPolicy Issue Resolved.

--- src/main/java/com/interviewmaster/Controller/PreparationController.java
+++ src/main/java/com/interviewmaster/Controller/PreparationController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/prep")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PreparationController {
 
     @Autowired
