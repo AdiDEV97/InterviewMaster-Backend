041d98c074e203dcef4a31944bd42b92349942b7
Adesh Vaidya <adeshvaidya97@gmail.com>
2024-06-01 11:14:07 +0530

    Test Case For Preparation Repository

--- src/test/java/com/interviewmaster/InterviewMasterApplicationTests.java
+++ src/test/java/com/interviewmaster/InterviewMasterApplicationTests.java
@@ -1,10 +1,17 @@
 package com.interviewmaster;
 
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class InterviewMasterApplicationTests {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     @Test
     void contextLoads() {
