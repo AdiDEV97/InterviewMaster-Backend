6ee5c2727158530307260139fa0f47d40c1b52b5
Adesh Vaidya <adeshvaidya97@gmail.com>
2024-06-03 12:01:47 +0530

    Test Case For Preparation Repository

--- src/main/java/com/interviewmaster/Payload/PreparationDto.java
+++ src/main/java/com/interviewmaster/Payload/PreparationDto.java
@@ -2,12 +2,16 @@ package com.interviewmaster.Payload;
 
 import com.interviewmaster.Model.Category;
 import com.interviewmaster.Model.InterviewRequisite;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PreparationDto {
 
     private int id;
