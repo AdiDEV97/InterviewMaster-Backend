68853cc778e53faa507ab4b9b73f56c323c28ff6
Adesh Vaidya <adeshvaidya97@gmail.com>
2024-06-06 17:48:33 +0530

    Test Case For Category Controller

--- src/test/java/com/interviewmaster/Controller/CategoryControllerTest.java
+++ src/test/java/com/interviewmaster/Controller/CategoryControllerTest.java
@@ -1,12 +1,9 @@
 package com.interviewmaster.Controller;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interviewmaster.Model.Category;
 import com.interviewmaster.Payload.CategoryDto;
 import com.interviewmaster.Service.Impl.CategoryServiceImpl;
-import com.mysql.cj.xdevapi.JsonParser;
-import org.hibernate.criterion.Example;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,25 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.StreamingHttpOutputMessage;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.MatchResult;
 
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*; // get() method
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 @WebMvcTest(CategoryController.class) // Load Application Context ---> For which class you want to write test cases
 public class CategoryControllerTest {
@@ -81,8 +69,6 @@ public class CategoryControllerTest {
     public void testGetCategoryById() throws Exception {
         when(this.catServ.categoryById(1)).thenReturn(categoryDto1);
 
-        //ResultActions c = this.mockMvc.perform(get("/api/v1/category/id-1")).andDo(print());
-        //System.out.println("---------------- C - " + (CategoryDto)c);
         this.mockMvc.perform(get("/api/v1/category/id-1")).andDo(print()).andExpect(jsonPath("$.categoryId").value(1));
         this.mockMvc.perform(get("/api/v1/category/id-1")).andExpect(jsonPath("$.categoryTitle").value("Java"));
         this.mockMvc.perform(get("/api/v1/category/id-1")).andExpect(jsonPath("$.categoryDescription").value("All Java Questions"));
