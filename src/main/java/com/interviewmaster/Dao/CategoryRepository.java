2ad3868b53b313cfb25e9e420c4b62c63a712b8a
Adesh Vaidya <adeshvaidya97@gmail.com>
2024-06-13 13:45:22 +0530

    Test Case For Preparation Service Impl

--- src/main/java/com/interviewmaster/Dao/CategoryRepository.java
+++ src/main/java/com/interviewmaster/Dao/CategoryRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+//Category Repository Interface
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 }
